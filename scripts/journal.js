30626695fb48d1ab0fd7d9314f64bc537c2533bc
Ronald Lankford <rolankford@gmail.com>
2020-04-20 11:41:24 -0500

    creating objects inside arrays

---
+++ scripts/journal.js
@@ -0,0 +1,18 @@
+const journalEntry = {
+
+};
+
+const journalEntryArr = [];
+
+journalEntryArr.push(journalEntry);
+
+const jsEntry = {
+    skillsToLearn: "DOM API manipulation"
+};
+
+const htmlEntry = {
+
+};
+
+journalEntryArr.push(jsEntry);
+journalEntryArr.push(htmlEntry);
